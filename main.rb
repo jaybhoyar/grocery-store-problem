33a7fc1d85b875c9bd962a8d6731aa565af7b4a9
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-12-14 15:09:19 +0530

    Add method to get item quantity

--- main.rb
+++ main.rb
@@ -2,7 +2,16 @@
 def accept_user_items
   puts "Please enter all the items purchased separated by a comma"
   items_bought = gets.chomp.split(',')
-  puts items_bought
+  
+  create_item "milk", items_bought if items_bought.include?('milk')
+  create_item "bread", items_bought if items_bought.include?('milk')
+  create_item "banana", items_bought if items_bought.include?('banana')
+  create_item "apple", items_bought if items_bought.include?('apple')  
+end
+
+def create_item item_name, items_bought
+  quantity = items_bought.count(item_name)
+  puts quantity
 end
 
 accept_user_items
