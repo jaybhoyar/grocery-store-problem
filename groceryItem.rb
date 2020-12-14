9f7d1666e9b1fc7c99162ff3d04a54cf5b693ad2
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-12-14 23:49:45 +0530

    Add method to calculate price before discount

--- groceryItem.rb
+++ groceryItem.rb
@@ -1,12 +1,6 @@
 class GroceryItem
-  attr_accessor :item_name, :item_quantity, :item_price
+  attr_accessor :item_name, :item_quantity, :item_price_after_discount, :item_price_before_discount
   def initialize(item_name, item_quantity)
-    @item_name = item_name
-    @item_quantity = item_quantity
-    @item_price = calculate_item_price  
-  end
-  
-  def calculate_item_price
     @items_details = {
       "milk" => {  
         "unit_price" => 3.97,
@@ -22,15 +16,21 @@ class GroceryItem
         "unit_price" => 0.99 
       }
     }
-
+    @item_name = item_name
+    @item_quantity = item_quantity
+    @item_price_after_discount = calculate_item_price_after_discount
+    @item_price_before_discount = calculate_item_price_before_discount  
+  end
+  
+  def calculate_item_price_after_discount
     case @item_name
     when "milk"
       sale_on_quantity = 2
-      final_milk_price = calculate_price_after_discount(@items_details[@item_name]["unit_price"], @items_details[@item_name]["sale_unit_price"],sale_on_quantity)
+      final_milk_price = discount_calculator(@items_details[@item_name]["unit_price"], @items_details[@item_name]["sale_unit_price"],sale_on_quantity)
       return final_milk_price 
     when "bread"
       sale_on_quantity = 3
-      final_bread_price = calculate_price_after_discount(@items_details[@item_name]["unit_price"], @items_details[@item_name]["sale_unit_price"],sale_on_quantity)
+      final_bread_price = discount_calculator(@items_details[@item_name]["unit_price"], @items_details[@item_name]["sale_unit_price"],sale_on_quantity)
       return final_bread_price
     when "banana"
       return @items_details[@item_name]["unit_price"]
@@ -39,7 +39,11 @@ class GroceryItem
     end
   end
 
-  def calculate_price_after_discount(item_unit_price, item_sale_unit_price, sale_on_quantity)
+  def calculate_item_price_before_discount
+    return @items_details[@item_name]["unit_price"] * @item_quantity
+  end
+
+  def discount_calculator(item_unit_price, item_sale_unit_price, sale_on_quantity)
     item_price_after_discount = 0 
     if @item_quantity < sale_on_quantity
       item_price_after_discount = item_unit_price * @item_quantity
--- main.rb
+++ main.rb
@@ -7,18 +7,19 @@ def accept_user_items
   puts "Please enter all the items purchased separated by a comma"
   items_bought = gets.chomp.split(',')
   
-  create_item "milk", items_bought if items_bought.include?('milk')
-  create_item "bread", items_bought if items_bought.include?('bread')
-  create_item "banana", items_bought if items_bought.include?('banana')
-  create_item "apple", items_bought if items_bought.include?('apple')  
+  create_item("milk", items_bought) if items_bought.include?('milk')
+  create_item( "bread", items_bought) if items_bought.include?('bread')
+  create_item("banana", items_bought) if items_bought.include?('banana')
+  create_item("apple", items_bought) if items_bought.include?('apple')  
   puts "#{$all_prices_after_discount.sum}"
-  puts "#{$all_prices_before_discount.sum}"
+  puts "#{$all_prices_before_discount.sum.round(2) - $all_prices_after_discount.sum.round(2)}"
 end
 
-def create_item item_name, items_bought
+def create_item(item_name, items_bought)
   quantity = items_bought.count(item_name)
   single_item = GroceryItem.new(item_name, quantity)
-  $all_prices_after_discount.push(single_item.item_price)
+  $all_prices_after_discount.push(single_item.item_price_after_discount)
+  $all_prices_before_discount.push(single_item.item_price_before_discount)
 end
 
 accept_user_items
